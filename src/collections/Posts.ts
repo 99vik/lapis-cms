4fc576d3477b0da89787f45604e1bec0877c68ad
Viktor Miric <viktorm47@gmail.com>
2024-04-07 18:17:02 +0200

    Enabled reading posts with API for user or correct API key

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -2,6 +2,13 @@ import { CollectionConfig } from 'payload/types';
 
 const Posts: CollectionConfig = {
   slug: 'posts',
+  access: {
+    read: ({ req }) => {
+      if (req.user || req.headers.authorization === process.env.API_KEY)
+        return true;
+      return false;
+    },
+  },
   fields: [
     {
       name: 'title',
